f8c9cd121779d03fccbbc5f3f5f86f4f0e383514
Shirotha <zimmermann-sven@online.de>
2021-11-11 00:19:53 +0100

    added more potentials

--- common/la.hpp
+++ common/la.hpp
@@ -7,7 +7,6 @@
 #include <petsc.h>
 #include <slepceps.h>
 
-
 static PetscErrorCode ierr;
 #define E(X) do { ierr = (X); if (PetscUnlikely(ierr)) { PetscError(PETSC_COMM_SELF,__LINE__,PETSC_FUNCTION_NAME,__FILE__,ierr,PETSC_ERROR_REPEAT," "); throw ierr; } } while(0)
 #define PRINT(format, args...) E(PetscPrintf(PETSC_COMM_WORLD, format "\n", args))
@@ -36,23 +35,139 @@ namespace la
         throw "unsupported fd order";
     } 
 
+    template<typename T>
+    struct Triple
+    {
+        T x{};
+        T y{};
+        T z{};
+
+        Triple() = default;
+        Triple(T _x) : x(_x) {}
+        Triple(T _x, T _y) : x(_x), y(_y) {}
+        Triple(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
+
+        operator T() noexcept
+        {
+            return x;
+        }
+    };
+
+    typedef std::function<PetscScalar(PetscReal)> Potential;
+
+    struct SpecializedPotential
+    {
+        Potential potential;
+        std::pair<Triple<PetscReal>, Triple<PetscReal>> range;
+
+        SpecializedPotential(Potential pot, std::pair<Triple<PetscReal>, Triple<PetscReal>> rng) : potential(pot), range(rng) {}
+    };
+
+    template<typename... Ts>
+    struct PotentialFamily
+    {
+        virtual Potential get(Ts... args) = 0;
+        virtual std::pair<Triple<PetscReal>, Triple<PetscReal>> range(Ts ...args) = 0;
+        virtual SpecializedPotential operator()(Ts ...args)
+        {
+            return { get(args...), range(args...) };
+        }
+    };
+
+    struct Harmonic : PotentialFamily<PetscReal>
+    {
+        Potential get(PetscReal omega)
+        {
+            const double c = 0.5 * omega * omega;
+            return [c](PetscReal x) 
+            { 
+                return c * x * x; 
+            };
+        }
+        std::pair<Triple<PetscReal>, Triple<PetscReal>> range(PetscReal omega)
+        {
+            constexpr PetscReal threshold = 4.0;
+            PetscReal x = threshold / sqrt(omega);
+            return { -x, x };
+        }
+    } harmonic;
+
+    struct Coulomb : PotentialFamily<PetscReal>
+    {
+        Potential get(PetscReal amp)
+        {
+            auto r = range(amp);
+            PetscReal min = -200.0 / (r.second.x - r.first.x);
+            return [amp, min](PetscReal x) 
+            { 
+                PetscReal y = -amp / abs(x);
+                if (y < min)
+                    return min;
+                return y;
+            };
+        }
+        std::pair<Triple<PetscReal>, Triple<PetscReal>> range(PetscReal amp)
+        {
+            constexpr PetscReal threshold = 1e-1;
+            PetscReal x = sqrt(1/threshold * amp);
+            return { -x, x };
+        }
+    } coulomb;
+
+    struct Morse : PotentialFamily<PetscReal, PetscReal>
+    {
+        Potential get(PetscReal r0, PetscReal depth)
+        {
+            PetscReal a = 1.0 / sqrt(2 * depth);
+            return [r0, depth, a](PetscReal x)
+            {
+                PetscReal e = 1 - exp(-a * (x - r0));
+                return depth * e * e;
+            };
+        }
+        std::pair<Triple<PetscReal>, Triple<PetscReal>> range(PetscReal r0, PetscReal depth)
+        {
+            constexpr PetscReal threshold = 0.99;
+            return { 0.0, sqrt(2 * depth) * log(1.0 / (1 - sqrt(threshold))) };
+        }
+    } morse;
+
+    struct DoubleWell : PotentialFamily<PetscReal, PetscReal>
+    {
+        Potential get(PetscReal h, PetscReal c)
+        {
+            PetscReal h4_4 = 0.25 * h * h * h * h, c2_2 = 0.5 * c * c;
+            return [h4_4, c2_2](PetscReal x)
+            {
+                PetscReal x2 = x * x;
+                return c2_2 * x2 * x2 - h4_4 * x2;
+            };
+        }
+        std::pair<Triple<PetscReal>, Triple<PetscReal>> range(PetscReal h, PetscReal c)
+        {
+            constexpr PetscReal threshold = 0.6;
+            PetscReal x = threshold * sqrt(1 + sqrt(3)) * h * h / c;
+            return { -x, x };
+        }
+    } doubleWell;
+
     struct Grid
     {
         ::DM dm;
         PetscInt dimension;
-        DMBoundaryType boundary;
+        Triple<DMBoundaryType> boundary;
 
-        PetscInt size;
+        Triple<PetscInt> size;
         PetscInt dof;
         PetscInt stencil;
 
-        PetscReal min;
-        PetscReal max;
+        Triple<PetscReal> min;
+        Triple<PetscReal> max;
 
-        PetscReal step;
+        Triple<PetscReal> step;
 
-        PetscInt begin;
-        PetscInt end;
+        Triple<PetscInt> begin;
+        Triple<PetscInt> end;
 
         Grid(const DMBoundaryType boundary, const PetscInt count, const PetscInt dof, const PetscInt stencil, const PetscReal min, const PetscReal max) :
             dimension(1), boundary(boundary), size(count), dof(dof), stencil(stencil), min(min), max(max), step((max - min) / count)
@@ -65,6 +180,44 @@ namespace la
             E(DMDAGetLocalInfo(dm, &info));
             begin = info.xs;
             end = info.xs + info.mx;
+        }
+
+        Grid(const DMBoundaryType boundaryx, const DMBoundaryType boundaryy, const PetscInt countx, const PetscInt county, const PetscInt dof, const PetscInt stencil, const PetscReal minx, const PetscReal maxx, const PetscReal miny, const PetscReal maxy) :
+            dimension(2), boundary(boundaryx, boundaryy), size(countx, county), dof(dof), stencil(stencil), min(minx, miny), max(maxx, maxy), step((maxx - minx) / countx, (maxy - miny) / county)
+        {
+            E(DMDACreate2d(PETSC_COMM_WORLD, boundaryx, boundaryy, DMDA_STENCIL_BOX, countx, county, PETSC_DECIDE, PETSC_DECIDE, dof, stencil, NULL, NULL, &dm));
+            E(DMSetFromOptions(dm));
+            E(DMSetUp(dm));
+            E(DMDASetUniformCoordinates(dm, minx, maxx, miny, maxy, miny, maxy));
+            DMDALocalInfo info;
+            E(DMDAGetLocalInfo(dm, &info));
+            begin = { info.xs, info.ys };
+            end = { info.xs + info.mx, info.ys + info.my };
+        }
+
+        Grid(const DMBoundaryType boundaryx, const DMBoundaryType boundaryy, const DMBoundaryType boundaryz, const PetscInt countx, const PetscInt county, const PetscInt countz, const PetscInt dof, const PetscInt stencil, const PetscReal minx, const PetscReal maxx, const PetscReal miny, const PetscReal maxy, const PetscReal minz, const PetscReal maxz) :
+            dimension(3), boundary(boundaryx, boundaryy, boundaryz), size(countx, county, countz), dof(dof), stencil(stencil), min(minx, miny, minz), max(maxx, maxy, maxz), step((maxx - minx) / countx, (maxy - miny) / county, (maxz - minz) / countz)
+        {
+            E(DMDACreate3d(PETSC_COMM_WORLD, boundaryx, boundaryy, boundaryz, DMDA_STENCIL_BOX, countx, county, countz, PETSC_DECIDE, PETSC_DECIDE, PETSC_DECIDE, dof, stencil, NULL, NULL, NULL, &dm));
+            E(DMSetFromOptions(dm));
+            E(DMSetUp(dm));
+            E(DMDASetUniformCoordinates(dm, minx, maxx, miny, maxy, minz, maxz));
+            DMDALocalInfo info;
+            E(DMDAGetLocalInfo(dm, &info));
+            begin = { info.xs, info.ys, info.zs };
+            end = { info.xs + info.mx, info.ys + info.my, info.zs + info.mz };
+        }
+
+        std::shared_ptr<PetscScalar[]> getPoints() const
+        {
+            PetscScalar* ptr;
+            E(DMDAGetCoordinateArray(dm, &ptr));
+            std::shared_ptr<PetscScalar[]> buffer(ptr, [this](PetscScalar* ptr) 
+            {  
+                E(DMDARestoreCoordinateArray(this->dm, &ptr));
+            });
+            
+            return buffer;
         }
 
         ~Grid()
@@ -119,10 +272,10 @@ namespace la
                 }
             }
         }
-        
+        // TODO: support 2d, 3d
         void apply_diagonal(const InsertMode insert, const std::shared_ptr<Vector> diag);
-
-        void apply_diagonal(const InsertMode insert, const std::function<PetscScalar(PetscReal)>&& func);
+        // TODO: support 2d, 3d
+        void apply_diagonal(const InsertMode insert, const Potential&& func);
 
         void assemble(const MatAssemblyType type)
         {
@@ -147,6 +300,18 @@ namespace la
             E(VecAssemblyEnd(vec));
         }
 
+        std::shared_ptr<PetscScalar[]> getArray()
+        {
+            PetscScalar* ptr;
+            E(VecGetArray(vec, &ptr));
+            std::shared_ptr<PetscScalar[]> buffer(ptr, [this](PetscScalar* ptr)
+            {
+                E(VecRestoreArray(this->vec, &ptr));
+            });
+
+            return buffer;
+        }
+
         ~Vector()
         {
             ierr = VecDestroy(&vec);
@@ -158,12 +323,25 @@ namespace la
         ::EPS eps;
         std::shared_ptr<Matrix> matrix;
 
-        EigenSolver(std::shared_ptr<Matrix> mat, const EPSProblemType type) : matrix(mat)
+        EigenSolver(std::shared_ptr<Matrix> mat, const EPSProblemType type, const PetscInt dimension) : matrix(mat)
         {
             E(EPSCreate(PETSC_COMM_WORLD, &eps));
             E(EPSSetOperators(eps, matrix->mat, NULL));
             E(EPSSetProblemType(eps, type));
-            E(EPSSetWhichEigenpairs(eps, EPS_SMALLEST_REAL));
+            E(EPSSetType(eps, EPSARNOLDI));
+            /*RG rg;
+            E(EPSGetRG(eps, &rg));
+            E(RGSetType(rg, RGINTERVAL));
+            E(RGIntervalSetEndpoints(rg, 0.0, 5.0, -0.1, 0.1));
+            */
+            E(EPSSetWhichEigenpairs(eps, EPS_SMALLEST_REAL/*EPS_TARGET_REAL*//* EPS_ALL*/));
+            E(EPSSetTarget(eps, 0.5));
+            
+            E(EPSSetDimensions(eps, dimension, PETSC_DEFAULT, PETSC_DEFAULT));
+            //E(EPSKrylovSchurSetDimensions(eps, dimension, PETSC_DEFAULT, PETSC_DEFAULT));
+            E(EPSSetTolerances(eps, 1e-10, 10000));
+            //E(EPSSetTrueResidual(eps, PETSC_TRUE));
+            E(EPSSetConvergenceTest(eps, EPS_CONV_REL));
             E(EPSSetFromOptions(eps));
         }
 
@@ -220,19 +398,19 @@ namespace la
         }
     };
 
+    // TODO: support 2d, 3d
     void la::Matrix::apply_diagonal(const InsertMode insert, const std::shared_ptr<la::Vector> diag)
     {
         E(MatDiagonalSet(mat, diag->vec, insert));
     }
-
-    void Matrix::apply_diagonal(const InsertMode insert, const std::function<PetscScalar(PetscReal)>&& func)
+    // TODO: support 2d, 3d
+    void Matrix::apply_diagonal(const InsertMode insert, const Potential&& func)
     {
         Vector vec(grid);
-        PetscReal x = grid->min;
+        auto points = grid->getPoints();
         for (int i = 0; i < grid->size; ++i)
         {
-            E(VecSetValue(vec.vec, i, func(x), INSERT_VALUES));
-            x += grid->step;
+            E(VecSetValue(vec.vec, i, func(points[i].real()), INSERT_VALUES));
         }
         vec.assemble();
 
--- ex2/ex2.cpp
+++ ex2/ex2.cpp
@@ -1,30 +1,25 @@
+#include <sstream>
+#include <limits>
+#include <sciplot/sciplot.hpp>
+namespace plt = sciplot;
+
 #include "../common/bm.hpp"
 #include "../common/la.hpp"
 
-std::function<PetscScalar(PetscReal)> harmonic(PetscReal omega)
+std::shared_ptr<la::EigenSolver> solve_schrödinger1d(bm::BM& bm, const PetscInt n, const PetscInt dimension, const la::SpecializedPotential& potential)
 {
-    double c = 0.5 * omega * omega;
-    return [c](PetscReal x) { return c * x * x; };
-}
-
-std::shared_ptr<la::EigenSolver> solve_schrödinger(bm::BM& bm, const PetscInt n, const PetscReal range, const std::function<PetscScalar(PetscReal)>&& potential)
-{
-    PRINT("Range = %5E", range);
-
-    auto grid = std::make_shared<la::Grid>(DM_BOUNDARY_GHOSTED, n, 1, 1, -range, range);
+    PRINT("Range = %5E..%5E", potential.range.first.x, potential.range.second.x);
+    
+    auto grid = std::make_shared<la::Grid>(DM_BOUNDARY_GHOSTED, n, 1, 1, potential.range.first.x, potential.range.second.x);
     auto H = std::make_shared<la::Matrix>(grid);
-    PRINT("Step = %5E", grid->step);
+    PRINT("Step = %5E", grid->step.x);
     
     H->apply_cfd<2, 2>(ADD_VALUES, -0.5);
-    H->apply_diagonal(ADD_VALUES, std::move(potential));
-
-    H->assemble(MAT_FLUSH_ASSEMBLY);
-
+    H->apply_diagonal(ADD_VALUES, std::move(potential.potential));
 
     H->assemble(MAT_FINAL_ASSEMBLY);
-    //MatScale(H->mat, omega * grid->step * grid->step * 0.25);
 
-    auto solver = std::make_shared<la::EigenSolver>(H, EPS_HEP);
+    auto solver = std::make_shared<la::EigenSolver>(H, EPS_HEP, dimension);
 
     bm.lap();
     
@@ -33,6 +28,105 @@ std::shared_ptr<la::EigenSolver> solve_schrödinger(bm::BM& bm, const PetscInt n
     bm.lap();
 
     return solver;
+}
+
+template<typename T>
+void plot_eigenvectors(const std::shared_ptr<la::EigenSolver> solver, const T& potential, bool logScale=false)
+{
+    int i, j, n = solver->getNumEigenpairs();
+    PRINT("Converged Eigenvalues = %i", n);
+
+    if (n > 0)
+    {
+        plt::Plot plot;
+        plot.size(1300, 650);
+
+        plot.xlabel("x");
+        plot.ylabel("E - V_{min}");
+
+        plot.xrange(potential.range.first.x, potential.range.second.x);
+        
+        plot.legend()
+            .atOutsideBottom()
+            .displayHorizontal()
+            .displayHorizontalMaxCols(2);
+        
+        auto points = solver->matrix->grid->getPoints();
+        plt::Vec xs(solver->matrix->grid->size);
+        for (i = 0; i < xs.size(); ++i)
+            xs[i] = points[i].real();
+
+        std::stringstream label;
+        label.setf(label.scientific, label.floatfield);
+
+        EPSConvergedReason reason;
+        PetscScalar ev;
+        PetscReal error;
+        auto x = std::make_shared<la::Vector>(solver->matrix->grid);
+        plt::Vec pot(solver->matrix->grid->size);
+        double ymin = std::numeric_limits<double>::max(), ymax = 0, tmp, max, pmin = std::numeric_limits<double>::max();
+        for (i = 0; i < xs.size(); ++i)
+        {
+            tmp = potential.potential(xs[i]).real();
+            /*
+            if (tmp > ymax)
+                ymax = tmp;
+            if (tmp < ymin)
+                ymin = tmp;
+            */
+           if (tmp < pmin)
+            pmin = tmp;
+            pot[i] = tmp;
+        }
+        for (i = 0; i < pot.size(); ++i)
+            pot[i] -= pmin;
+
+        if (logScale)
+            for (i = 0; i < pot.size(); ++i)
+                pot[i] = log10(pot[i]);
+
+
+        plot.drawCurveFilled(xs, pot).labelNone().above().fillIntensity(0.3);
+
+        plt::Vec ys(solver->matrix->grid->size);
+        for (i = 0; i < n; ++i)
+        {
+            solver->getEigenpair(i, ev, *x, error);
+            auto vec = x->getArray();
+            max = 0;
+            for (j = 0; j < ys.size(); ++j)
+            {
+                tmp = pow(std::__complex_abs(vec[j]), 2);
+                //tmp = vec[j].real();
+                if (tmp > max)
+                    max = tmp;
+                ys[j] = tmp;
+            }
+            max = 1.0 / max;
+            for (j = 0; j < ys.size(); ++j)
+            {
+                tmp = ys[j] * max + ev.real() - pmin;
+                if (logScale)
+                    tmp = log10(tmp);
+                if (tmp > ymax)
+                    ymax = tmp;
+                if (tmp < ymin)
+                    ymin = tmp;
+                ys[j] = tmp;
+            }
+
+            label << ev.real() << " + " << ev.imag() << "i +/- " << error;
+
+            E(EPSGetConvergedReason(solver->eps, &reason));
+            PRINT("%9E + %9Ei +/- %9E (%i)", ev.real(), ev.imag(), error, reason);
+            plot.drawCurve(xs, ys).label(label.str());
+
+            label.str("");
+        }
+        
+        plot.yrange(ymin - 0.01 * (ymax - ymin), ymax);
+        plot.show();
+    }
 }
 
 int main(int argc, char* argv[])
@@ -46,56 +140,47 @@ int main(int argc, char* argv[])
         MPI_Comm_size(PETSC_COMM_WORLD, &size);
         MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
         PRINT("MPI Size = %i, MPI Rank = %i", size, rank);
-        PetscInt n=199;
+        PetscInt n=200;
         E(PetscOptionsGetInt(NULL, NULL, "-n", &n, NULL));
         PRINT("Matrix Size = %d", n);
-
-        PetscReal omega = 4.0;
-        auto solver = solve_schrödinger(bm, n, 4.0 / sqrt(omega), harmonic(omega));
-
-        PRINT("Iterations = %i", solver->getNumIterations());
-        //PRINT("Type = %i", solver->getType());
-        //PRINT("Result Dimension = %i", solver->getDimension());
-        
-        /*{
-            auto [iter, tol] = solver->getTolerance();
-            PRINT("Tolerance = %.4E, Max. Iterations = %i", iter, tol);
-        }*/
-
+        //if (0)
         {
-            int i, n = solver->getNumEigenpairs();
-            PRINT("Converged Eigenvalues = %i", n);
+            auto potential = la::harmonic(1.0);
+            
+            auto solver = solve_schrödinger1d(bm, n, 4, potential);
+            PRINT("Iterations = %i", solver->getNumIterations());
+            plot_eigenvectors(solver, potential);
+        }
+        //if (0)
+        {
+            auto potential = la::coulomb(8.0);
 
-            if (n > 0)
-            {
-                PetscScalar ev;
-                PetscReal error;
-                auto x = std::make_shared<la::Vector>(solver->matrix->grid);
-                
-                for (i = 0; i < n; ++i)
-                {
-                    solver->getEigenpair(i, ev, *x, error);
-                    PRINT("%9E + %9Ei +/- %12E", ev.real(), ev.imag(), error);
-                    /*
-                    PetscScalar* vs = (PetscScalar*)malloc(sizeof(PetscScalar) * grid->size);
-                    VecGetArray(x->vec, &vs);
-                    for (int j = 0; j < grid->size; ++j)
-                    {
-                        PRINT("%9E", vs[j].real() * vs[j].real() + vs[j].imag() * vs[j].imag());
-                    }
-                    VecRestoreArray(x->vec, &vs);
-                    free(vs);
-                    */
-                }
-            }
+            auto solver = solve_schrödinger1d(bm, n, 8, potential);
+            PRINT("Iterations = %i", solver->getNumIterations());
+            plot_eigenvectors(solver, potential/*, true*/);
+        }
+        //if (0)
+        {
+            auto potential = la::morse(2.0, 4.0);
+
+            auto solver = solve_schrödinger1d(bm, n, 8, potential);
+            plot_eigenvectors(solver, potential);
+        }
+        //if (0)
+        {
+            // TODO: find better parameters
+            auto potential = la::doubleWell(1.1, 0.2);
+
+            auto solver = solve_schrödinger1d(bm, n, 4, potential);
+            plot_eigenvectors(solver, potential);
         }
     }
     bm.stop();
     /*
     PRINT("Setup = %9E +/- %9E", bm[0], bm.getError());
     PRINT("Solve = %9E +/- %9E", bm[1], bm.getError());
-    PRINT("Total = %9E +/- %9E", bm.getTotal(), bm.getError());
     */
+    PRINT("Total = %9E +/- %9E", bm.getTotal(), bm.getError());
     E(SlepcFinalize());
 
     return ierr;
